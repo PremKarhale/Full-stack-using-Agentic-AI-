0f5909149a4e0bd7790dd7019ea6efbee6741237
Prem Karhale <premkarhale383@gmail.com>
2026-01-11 11:14:09 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from prompt_utils import clean_text, is_long_prompt
-cleaned_text=clean_text('Write a detailed blog on Generative AI in simple words ')
-print(cleaned_text)
-
-islong=is_long_prompt('Write a detailed blog on Generative AI in simple words')
-print(islong)
-print(len('Write a detailed blog on Generative AI in simple words'))
